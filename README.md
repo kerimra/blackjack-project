dae2bc93ddaa6950c27afb987b795bc7198fe93c
kerimra <kerimra125@gmail.com>
2023-05-03 15:58:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# BLACK JACK
+
+### `Black Jack Game - With working leaderboard`
+
+Complete matching game done with VueJS
+
+
+## Built With
+* VueJs (Front-End)
+* Vuetify (Styling Framework)
+* Strapi Headless CMS (Back-End)
+
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
---
+++ components/Leaderboard.vue
@@ -0,0 +1,30 @@
+<template>
+     
+      <v-data-table 
+        :headers="headers"
+        :items="players"
+        :items-per-page="10"
+        item-class="secondary"
+        sort-by="bank"
+        :sort-desc="true"
+        dense
+        key="name"
+        mobile-breakpoint="200px"
+        class="elevation-1"
+      /> 
+      
+      
+</template>
+
+<script>
+export default {
+
+  props: {
+    headers: [],
+    players: []
+  },
+    methods:{  
+  
+  }
+}
+</script>
